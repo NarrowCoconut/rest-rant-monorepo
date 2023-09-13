d6b075daa4c2a816954b762284d7767cb6b23a87
Dustin Smith <45208269+NarrowCoconut@users.noreply.github.com>
2023-09-12 20:31:56 -0400

    prep for aws

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,21 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 const app = express();
+
 
 // Express Settings
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+
+// serve static front end in production mode
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'public', 'build')));
+}
 
 // Controllers & Routes
 
